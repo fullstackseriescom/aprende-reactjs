a5ecbdf04a40ffdd71110795b5018c45b88dffc6
vreaxe <vreaxe@gmail.com>
2019-03-05 22:20:31 +0100

    Código Post 3

---
+++ 03-propiedades/src/components/PostList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PostItem from "./PostItem";
+
+class PostList extends React.Component {
+  render() {
+    return (
+      <div>
+        <PostItem
+          post={{
+            title: "Post Title 1",
+            content:
+              "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          }}
+        />
+        <PostItem
+          post={{
+            title: "Post Title 2",
+            content:
+              "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default PostList;
